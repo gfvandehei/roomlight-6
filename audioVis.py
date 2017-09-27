5c9f691182588bacb580018717c11c5a782d4f0f
gfvandehei <gfvandehei@gmail.com>
2017-09-27 19:48:14 -0400

    fully working audio visualizer, changed sensitivity and arduino end serial processing

--- audioVis.py
+++ audioVis.py
@@ -11,7 +11,7 @@ stream=p.open(format=pyaudio.paInt16,channels=1,rate=RATE,input=True,
 for i in range(int(10*44100/1024)): #go for a few seconds
     data = np.fromstring(stream.read(CHUNK),dtype=np.int16)
     peak=np.average(np.abs(data))*2
-    bars="#"*int(50*peak/2**16)
+    bars="#"*int(50*peak/2**14)
     print("%04d %05d %s"%(i,peak,bars))
 
 stream.stop_stream()
